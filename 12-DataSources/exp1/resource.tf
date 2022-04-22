093bdf359b8b004dab2b97d99494d7773715c1e1
Amit Vashist <amitvashist7@outlook.com>
2022-04-22 06:38:38 +0000

    12-DataSources

---
+++ 12-DataSources/exp1/resource.tf
@@ -0,0 +1,30 @@
+# Create a resource group
+resource "azurerm_resource_group" "frontend" {
+  name     = "frontend-RG"
+  location = "westus"
+  tags = { 
+     Owner = "DevOps Team" 
+     Project = "Terraform Automation"
+     Env    = "Production"
+ }
+}
+
+
+
+data "azurerm_resources" "example" { 
+  resource_group_name = "frontend-RG"
+  depends_on = [azurerm_resource_group.frontend]
+}
+
+
+
+resource "azurerm_resource_group" "backend" {
+  name     = "${data.azurerm_resources.example.id}-backend"
+  location = "eastus"
+  tags = { 
+     Owner = "DevOps Team" 
+     Project = "Terraform Automation"
+     Env    = "Production"
+ }
+}
+
