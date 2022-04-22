d21890f6278a5b9c3cf2cc84704a8c90d0ca90e6
Amit Vashist <amitvashist7@outlook.com>
2022-04-22 09:21:56 +0000

    13-Modules

---
+++ 13-Modules/modules/network/variable.tf
@@ -0,0 +1,30 @@
+variable "my_loc" {
+  description = "My Resource Location"
+  default = "eastus"
+}
+
+variable "my_vm_count" {
+  default = 1
+}
+
+variable "my_rg_name" {
+  description = "My Resource Group Name"
+  default     = "TestRG"
+}
+
+
+variable "my_vpc_cidr" { 
+   default = ["10.0.0.0/16"]
+}
+
+variable "my_vpc_subnet_cidr" { 
+   default = ["10.0.0.0/16"]
+}
+
+
+variable "inbound_ports" { 
+  type = list(string)
+  default = ["22","80","8080"]
+}
+
+
